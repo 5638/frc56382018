c5217399ebeeb5e75a28c8b1545dd7764c66d1f3
brandonlund <lundbra@lqpv.net>
2018-03-08 23:02:53 -0600

    df

--- src/org/usfirst/frc5638/frc56382018/commands/LEDcom.java
+++ src/org/usfirst/frc5638/frc56382018/commands/LEDcom.java
@@ -1,29 +1,21 @@
 package org.usfirst.frc5638.frc56382018.commands;
 
 import org.usfirst.frc5638.frc56382018.Robot;
-import org.usfirst.frc5638.frc56382018.RobotMap;
 import org.usfirst.frc5638.frc56382018.subsystems.LEDsub;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotState;
 import edu.wpi.first.wpilibj.command.Command;
 
-/**
- *
- */
 public class LEDcom extends Command {
 
     public LEDcom() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
     	requires(Robot.LEDsub);
     }
 
-    // Called just before this Command runs the first time
     protected void initialize() {
     }
 
-    // Called repeatedly when this Command is scheduled to run
     protected void execute(DriverStation.Alliance color) {
     	color = DriverStation.getInstance().getAlliance();
     	if(RobotState.isEnabled() && color == DriverStation.Alliance.Blue) {
@@ -39,17 +31,13 @@ public class LEDcom extends Command {
     	}
     }
 
-    // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
         return false;
     }
 
-    // Called once after isFinished returns true
     protected void end() {
     }
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
     protected void interrupted() {
     }
 }
--- src/org/usfirst/frc5638/frc56382018/subsystems/LEDsub.java
+++ src/org/usfirst/frc5638/frc56382018/subsystems/LEDsub.java
@@ -6,22 +6,14 @@ import org.usfirst.frc5638.frc56382018.commands.LEDcom;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-/**
- *
- */
 public class LEDsub extends Subsystem {
-
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
 	
 	private final static Spark LEDcontroller = RobotMap.LEDcontroller;
 	
     public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
         setDefaultCommand(new LEDcom());
     }
     public static void color(double color){
     	LEDcontroller.set(color);
     }
 }
-
